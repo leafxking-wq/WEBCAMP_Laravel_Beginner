31bb33b77d66b18abef8f9bdf0893f0f8015b18f
Rena <rena@example.com>
2025-11-05 03:00:56 +0000

    1Page目と2Page目を書いた

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [\App\Http\Controllers\WelcomeController::class, 'index']);
+Route::get('/welcome/second', [\App\Http\Controllers\WelcomeController::class, 'second']);
