5c75a3aa3186c02ea13d7529e3ace391cc20ee6e
Rena <rena@example.com>
2025-11-12 09:06:18 +0000

    完了タスク一覧を追加

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\CompletedTaskController;
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\Admin\AuthController as AdminAuthController;
 use App\Http\Controllers\Admin\HomeController as AdminHomeController;
@@ -20,7 +21,7 @@ use App\Http\Controllers\Admin\UserController as AdminUserController;
 |
 */
 
-//タスク管理システム
+// タスク管理システム
 Route::get('/', [AuthController::class, 'index'])->name('front.index');
 Route::post('/login', [AuthController::class, 'login']);
 // 認可処理
@@ -30,12 +31,13 @@ Route::middleware(['auth'])->group(function () {
         Route::post('/register', [TaskController::class, 'register']);
         Route::get('/detail/{task_id}', [TaskController::class, 'detail'])->whereNumber('task_id')->name('detail');
         Route::get('/edit/{task_id}', [TaskController::class, 'edit'])->whereNumber('task_id')->name('edit');
-        Route::put('/edit/{task_id}', [TaskController::class, 'editSave'])->whereNumber('task_id')->name('edit_save');
+        Route::put('/edit/{task_id}', [TaskController::class, 'editSave'])->whereNumber('task_id')->name('edit_save');       
         Route::delete('/delete/{task_id}', [TaskController::class, 'delete'])->whereNumber('task_id')->name('delete');
         Route::post('/complete/{task_id}', [TaskController::class, 'complete'])->whereNumber('task_id')->name('complete');
-        Route::get('/csv/download', [TaskController::class, 'csvDownload']);       
+        Route::get('/csv/download', [TaskController::class, 'csvDownload']);
     });
     Route::get('/logout', [AuthController::class, 'logout']);
+    Route::get('/completed_tasks/list', [CompletedTaskController::class, 'list']);
 });
 // 管理画面
 Route::prefix('/admin')->group(function () {
@@ -48,9 +50,9 @@ Route::prefix('/admin')->group(function () {
     });
 });
 
-//テスト用
-Route::get('/welcome', [\App\Http\Controllers\WelcomeController::class, 'index']);
-Route::get('/welcome/second', [\App\Http\Controllers\WelcomeController::class, 'second']);
+// テスト用
+Route::get('/welcome', [WelcomeController::class, 'index']);
+Route::get('/welcome/second', [WelcomeController::class, 'second']);
 // form入力テスト用
 Route::get('/test', [TestController::class, 'index']);
 Route::post('/test/input', [TestController::class, 'input']);
